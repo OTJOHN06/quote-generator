bec537528d7f7471d0f215266fedf34eec78ab44
OTJOHN06 <johnitater2@gmail.com>
2020-11-28 10:55:25 +0000

    New quote on refresh

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const quotecContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterButton = document.getElementById('twitter')
+const newQuoteButton = document.getElementById('new-quote')
+
 let apiQuotes = []
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote)
+    quoteText.textContent = quote.text
+    if (quote.author === null) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author
+    }
 }
 
 // Get quote from API
